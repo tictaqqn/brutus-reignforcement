72e5a951d24f15bcaa88b5df9a18968256f6b6ff
bababax11 <bababax11@example.com>
2019-12-27 15:26:35 +0900

    コマの表示

--- game/gui.py
+++ game/gui.py
@@ -25,12 +25,12 @@ class Frame(wx.Frame):
     def __init__(self):
         self.gs = GameState()
         window_title = 'Brutus'
-        window_size = (400, 440)
+        window_size = (250, 400)
         wx.Frame.__init__(self, None, -1, window_title, size=window_size)
         # panel
         self.panel = wx.Panel(self)
         # self.panel.Bind(wx.EVT_LEFT_DOWN, self.try_move)
-        # self.panel.Bind(wx.EVT_PAINT, self.refresh)
+        self.panel.Bind(wx.EVT_PAINT, self.refresh)
 
         menu = wx.Menu()
         menu.Append(1, u"New Game(Black)")
@@ -46,6 +46,39 @@ class Frame(wx.Frame):
 
         # status bar
         self.CreateStatusBar()
+
+    def refresh(self, event):
+        dc = wx.PaintDC(self.panel)
+        # self.update_status_bar()
+
+        w, h = self.panel.GetSize()
+        # background
+        dc.SetBrush(wx.Brush("light gray"))
+        dc.DrawRectangle(0, 0, w, h)
+        # grid
+        dc.SetBrush(wx.Brush("black"))
+        px, py = w / 5, h / 7
+        for y in range(8):
+            dc.DrawLine(y * px, 0, y * px, h)
+            dc.DrawLine(0, y * py, w, y * py)
+        dc.DrawLine(w - 1, 0, w - 1, h - 1)
+        dc.DrawLine(0, h - 1, w - 1, h - 1)
+
+        # stones
+        brushes = {
+            -2: wx.Brush("white"),
+            -1: wx.Brush("white"), 
+            1: wx.Brush("black"),
+            2: wx.Brush("black"),
+        }
+        
+        for i in range(7):
+            for j in range(5):
+                c = self.gs.board[i, j]
+                if c != 0:
+                    dc.SetBrush(brushes[c])
+                    dc.DrawEllipse(j * px, i * py, px, py)
+
 
 
 if __name__ == "__main__":
