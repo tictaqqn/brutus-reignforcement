74a362e3408f5931429d0bdd637626da5ee6d85e
bababax11 <bababax11@example.com>
2019-12-31 19:25:31 +0900

    quitの実装と移動変化の出力

--- game/gui.py
+++ game/gui.py
@@ -1,7 +1,8 @@
 from logging import getLogger
+import numpy as np
 import wx
 from wx.core import CommandEvent
-
+from .errors import *
 from .game_state import GameState
 
 logger = getLogger(__name__)
@@ -24,12 +25,14 @@ class Frame(wx.Frame):
 
     def __init__(self):
         self.gs = GameState()
+        self.logs = []
+        self.piece_selected = False
         window_title = 'Brutus'
         window_size = (250, 400)
         wx.Frame.__init__(self, None, -1, window_title, size=window_size)
         # panel
         self.panel = wx.Panel(self)
-        # self.panel.Bind(wx.EVT_LEFT_DOWN, self.try_move)
+        self.panel.Bind(wx.EVT_LEFT_DOWN, self.try_move)
         self.panel.Bind(wx.EVT_PAINT, self.refresh)
 
         menu = wx.Menu()
@@ -44,8 +47,49 @@ class Frame(wx.Frame):
         menu_bar.Append(menu, u"menu")
         self.SetMenuBar(menu_bar)
 
+        self.Bind(wx.EVT_MENU, self.handle_quit, id=9)
+
         # status bar
         self.CreateStatusBar()
+
+    def try_move(self, event):
+        event_x, event_y = event.GetX(), event.GetY()
+        w, h = self.panel.GetSize()
+
+        if not self.piece_selected:
+            self.piece_selected = True
+            self.selected_x = int(event_x / (w / 5))
+            self.selected_y = int(event_y / (h / 7))
+            return
+
+        x = int(event_x / (w / 5))
+        y = int(event_y / (h / 7))
+        if x == self.selected_x and y == self.selected_y:
+            self.piece_selected = False
+            return
+
+        if not (-1 <= x - self.selected_x <= 1 and -2 <= y - self.selected_y <= 2):
+            return
+
+        d = np.array([x - self.selected_x, y - self.selected_y])
+        print(d)
+        # try:
+        #     state = gs.move(y, x, drc)
+        # except GameError as e:
+        #     print(e)
+        #     print("入力が不正です。もう一度入力してください。")
+        #     return
+        # self.logs.append((i, j, d))
+        # if state == 1:
+        #     print(gs)
+        #     print("先手勝利")
+        #     return gs, logs
+        # elif state == -1:
+        #     print(gs)
+        #     print("後手勝利")
+        #     return gs, logs
+        self.piece_selected = False
+
 
     def refresh(self, event):
         dc = wx.PaintDC(self.panel)
@@ -78,6 +122,9 @@ class Frame(wx.Frame):
                 if c != 0:
                     dc.SetBrush(brushes[c])
                     dc.DrawEllipse(j * px, i * py, px, py)
+
+    def handle_quit(self, event: CommandEvent):
+        self.Close()
 
 
 if __name__ == "__main__":
