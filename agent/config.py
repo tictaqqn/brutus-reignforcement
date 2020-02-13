3379c208539f0bdddeebcc92732412da71f6937f
bababax11 <bababax11@example.com>
2020-02-13 23:08:31 +0900

    edit config

--- agent/config.py
+++ agent/config.py
@@ -17,7 +17,7 @@ class ModelConfig(ConfigBase):
         self.res_layer_num = 2
         self.l2_reg = 1e-4
         self.value_fc_size = 256
-        self.learning_rate = 0.01
+        self.learning_rate = 0.01 # もっと小さいほうがいい?
 
 class QLearnConfig(ConfigBase):
     
@@ -26,13 +26,12 @@ class QLearnConfig(ConfigBase):
 
         self.num_episodes = 300  # 総ゲーム数
         self.max_number_of_steps = 25  # 1ゲームの最大手数
-        self.goal_average_reward = 50  # この報酬を超えると学習終了
+        # self.goal_average_reward = 50  # この報酬を超えると学習終了
         self.num_consecutive_iterations = 10  # 学習完了評価の平均計算を行う試行回数
         self.save_interval = 50 # このゲーム数ごとに保存
         self.gamma = 0.99    # 割引係数
         # ---
         # self.hidden_size = 16               # Q-networkの隠れ層のニューロンの数
-        self.learning_rate = 0.00001        # Q-networkの学習係数
         self.memory_size = 10000            # バッファーメモリの大きさ
         self.batch_size = 32                # Q-networkを更新するバッチの大記載
         # ---
