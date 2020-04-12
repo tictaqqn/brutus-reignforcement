2717540547339356a9a2f4d129a30eac4a12b9b3
bababax11 <bababax11@example.com>
2020-04-12 20:14:04 +0900

    add dfpn

---
+++ uct/dfpn.py
@@ -0,0 +1,62 @@
+from typing import Union
+import math
+import time
+import copy
+import numpy as np
+
+from game.game_state import GameState, Winner
+from .uct_node import NodeHash, UctNode, UCT_HASH_SIZE, NOT_EXPANDED
+from agent.model_zero import ModelZero
+from agent.config import Config
+
+def dfpn(gs: GameState) -> Union[None, int]:
+    state = gs.get_winner()
+    if state != Winner.not_ended:
+        print('bestmove resign')
+        print(gs)
+        return None
+
+    turn = gs.turn
+    if turn == 1:
+        my_side, other_side = Winner.plus, Winner.minus
+    else:
+        my_side, other_side = Winner.minus, Winner.plus
+
+    
+    # 探索開始時刻の記録
+    # begin_time = time.time()
+    legal_moves = list(gs.generate_legal_moves())
+
+    if len(legal_moves) == 1:
+        return legal_moves[0]
+
+    for a in legal_moves:
+        state = gs.move_with_id(a)
+        if state == my_side:
+            return a
+        if state == other_side:
+            gs.pop()
+            continue
+
+        post_legal_moves = list(gs.generate_legal_moves())
+
+        all_win = True
+        for a2 in post_legal_moves:
+            state = gs.move_with_id(a2)
+
+            if state != my_side:
+                all_win = False
+                gs.pop()
+                break
+            gs.pop()
+
+        if all_win:
+            return a
+        
+        gs.pop()
+            
+    return None
+
+        
+
+        
